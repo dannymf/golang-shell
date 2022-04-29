3e40c8b8dc3d1ee82248f690bc8a448cf68100be
Daniel Friedman <dannymf0@gmail.com>
2022-04-29 01:45:11 -0400

    remove comments

--- cmd/pipes/main.go
+++ cmd/pipes/main.go
@@ -5,14 +5,5 @@ import (
 )
 
 func main() {
-	// testString1 := "echo Joseph"
-	// testString2 := "wc"
-
-	// lexed1, _ := shell_pipes.Lexer(testString1)
-	// parsed1, _ := shell_pipes.Parser(lexed1)
-
-	// lexed2, _ := shell_pipes.Lexer(testString2)
-	// parsed2, _ := shell_pipes.Parser(lexed2)
-	// shell_pipes.ArbitraryPipe(parsed1, parsed2)
 	pipes.MainLoop()
 }
--- cmd/shell/main.go
+++ cmd/shell/main.go
@@ -6,15 +6,4 @@ import (
 
 func main() {
 	shell.MainLoop()
-
-	// testString1 := "echo Joseph"
-	// testString2 := "wc"
-
-	// lexed1, _ := shell_pipes.Lexer(testString1)
-	// parsed1, _ := shell_pipes.Parser(lexed1)
-
-	// lexed2, _ := shell_pipes.Lexer(testString2)
-	// parsed2, _ := shell_pipes.Parser(lexed2)
-	// shell_pipes.ArbitraryPipe(parsed1, parsed2)
-
 }
